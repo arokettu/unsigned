70dc5da4487b313d9099e3b6100fcbeb9ce75217
Anton Smirnov <sandfox@sandfox.me>
2022-08-26 10:22:53 +0300

    Code style

--- src/lib.php
+++ src/lib.php
@@ -2,34 +2,36 @@
 
 /** @noinspection DuplicatedCode */
 
+declare(strict_types=1);
+
 namespace Arokettu\Unsigned;
 
 function from_int(int $value, int $sizeof): string
 {
-    $hex = dechex($value);
+    $hex = \dechex($value);
     if (\strlen($hex) > $sizeof * 2) {
         throw new \InvalidArgumentException("$value does not fit into $sizeof bytes");
     }
-    $hex = str_pad($hex, $sizeof * 2, $value >= 0 ? '0' : 'f', STR_PAD_LEFT);
+    $hex = \str_pad($hex, $sizeof * 2, $value >= 0 ? '0' : 'f', STR_PAD_LEFT);
 
     if (\strlen($hex) % 2 === 1) {
         $hex = '0' . $hex;
     }
-    return strrev(hex2bin($hex));
+    return \strrev(\hex2bin($hex));
 }
 
 function to_int(string $value): int
 {
-    $value = rtrim($value, "\0");
+    $value = \rtrim($value, "\0");
 
     if (
         \strlen($value) > PHP_INT_SIZE ||
-        \strlen($value) === PHP_INT_SIZE && ord($value[PHP_INT_SIZE - 1]) > 127
+        \strlen($value) === PHP_INT_SIZE && \ord($value[PHP_INT_SIZE - 1]) > 127
     ) {
-        throw new \DomainException('The value is larger than PHP integer');
+        throw new \RangeException('The value is larger than PHP integer');
     }
 
-    return hexdec(bin2hex(strrev($value)));
+    return \hexdec(\bin2hex(\strrev($value)));
 }
 
 function from_hex(string $value, int $sizeof): string
@@ -38,12 +40,12 @@ function from_hex(string $value, int $sizeof): string
         $s = $sizeof * 2;
         throw new \InvalidArgumentException("Hex value for \$sizeof == $sizeof must be $s chars long");
     }
-    return strrev(hex2bin($value));
+    return \strrev(\hex2bin($value));
 }
 
 function to_hex(string $value): string
 {
-    return bin2hex(strrev($value));
+    return \bin2hex(\strrev($value));
 }
 
 /**
@@ -57,12 +59,12 @@ function shift_left(string $value, int $shift): string
         throw new \InvalidArgumentException('$shift must be non negative');
     }
     if ($shift >= $sizeof * 8) {
-        return str_repeat("\0", $sizeof);
+        return \str_repeat("\0", $sizeof);
     }
     if ($shift >= 8) {
         $easyShift = \intdiv($shift, 8);
         $shift = $shift % 8;
-        $value = str_repeat("\0", $easyShift) . substr($value, 0, $sizeof - $easyShift);
+        $value = \str_repeat("\0", $easyShift) . \substr($value, 0, $sizeof - $easyShift);
     }
     if ($shift === 0) {
         return $value;
@@ -70,8 +72,8 @@ function shift_left(string $value, int $shift): string
 
     $carry = 0;
     for ($i = 0; $i < $sizeof; ++$i) {
-        $newChr = ord($value[$i]) << $shift | $carry;
-        $value[$i] = chr($newChr);
+        $newChr = \ord($value[$i]) << $shift | $carry;
+        $value[$i] = \chr($newChr);
         $carry = \intdiv($newChr, 256);
     }
 
@@ -89,12 +91,12 @@ function shift_right(string $value, int $shift): string
         throw new \InvalidArgumentException('$shift must be non negative');
     }
     if ($shift >= $sizeof * 8) {
-        return str_repeat("\0", $sizeof);
+        return \str_repeat("\0", $sizeof);
     }
     if ($shift >= 8) {
         $easyShift = \intdiv($shift, 8);
         $shift = $shift % 8;
-        $value = substr($value, $easyShift) . str_repeat("\0", $easyShift);
+        $value = \substr($value, $easyShift) . \str_repeat("\0", $easyShift);
     }
     if ($shift === 0) {
         return $value;
@@ -104,14 +106,14 @@ function shift_right(string $value, int $shift): string
     $carry = 0;
     $shift = 8 - $shift;
     for ($i = 0; $i < $sizeof; ++$i) {
-        $newChr = ord($value[$i]) << $shift | $carry;
-        $value[$i] = chr($newChr);
+        $newChr = \ord($value[$i]) << $shift | $carry;
+        $value[$i] = \chr($newChr);
         $carry = \intdiv($newChr, 256);
     }
 
-    $value[$i] = chr($carry);
+    $value[$i] = \chr($carry);
 
-    return substr($value, 1);
+    return \substr($value, 1);
 }
 
 function add(string $a, string $b): string
@@ -124,8 +126,8 @@ function add(string $a, string $b): string
 
     $carry = 0;
     for ($i = 0; $i < $sizeof; ++$i) {
-        $newChr = ord($a[$i]) + ord($b[$i]) + $carry;
-        $a[$i] = chr($newChr);
+        $newChr = \ord($a[$i]) + \ord($b[$i]) + $carry;
+        $a[$i] = \chr($newChr);
         $carry = \intdiv($newChr, 256);
     }
 
@@ -140,16 +142,16 @@ function mul(string $a, string $b): string
         throw new \InvalidArgumentException("Arguments must be the same size, $sizeof and $sizeofb bytes given");
     }
 
-    $newval = str_repeat("\0", $sizeof);
+    $newval = \str_repeat("\0", $sizeof);
 
     for ($i = 0; $i < $sizeof; $i++) {
         $carry = 0;
-        $ord = ord($a[$i]);
+        $ord = \ord($a[$i]);
         for ($j = 0; $j < $sizeof - $i; $j++) {
             $idx = $i + $j;
 
-            $newChr = $ord * ord($b[$j]) + ord($newval[$idx]) + $carry;
-            $newval[$idx] = chr($newChr);
+            $newChr = $ord * \ord($b[$j]) + \ord($newval[$idx]) + $carry;
+            $newval[$idx] = \chr($newChr);
             $carry = \intdiv($newChr, 256);
         }
     }
