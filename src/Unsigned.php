ff2c102f85b5615db1c783fcad80970dfeba6955
Anton Smirnov <sandfox@sandfox.me>
2025-03-27 08:28:58 +0200

    Prepare the class

--- src/Unsigned.php
+++ src/Unsigned.php
@@ -1,18 +1,12 @@
 <?php
 
-/**
- * @noinspection DuplicatedCode
- * @noinspection PhpFullyQualifiedNameUsageInspection
- */
-
 declare(strict_types=1);
 
-namespace Arokettu\Unsigned
-{
-    use Arokettu\Unsigned as u;
-    use Arokettu\Unsigned\Internal as i;
+namespace Arokettu\Unsigned;
 
-    function from_int(int $value, int $sizeof): string
+final class Unsigned
+{
+    public static function from_int(int $value, int $sizeof): string
     {
         $hex = \dechex($value);
         $strlen = \strlen($hex);
@@ -33,27 +27,27 @@ namespace Arokettu\Unsigned
         return \strrev(\hex2bin($hex));
     }
 
-    function to_int(string $value): int
+    public static function to_int(string $value): int
     {
-        if (!u\fits_into_int($value)) {
+        if (!self::fits_into_int($value)) {
             throw new \RangeException('The value is larger than PHP integer');
         }
 
         return \hexdec(\bin2hex(\strrev($value)));
     }
 
-    function to_signed_int(string $value): int
+    public static function to_signed_int(string $value): int
     {
         $sizeof = \strlen($value);
-        if (u\is_bit_set($value, $sizeof * 8 - 1)) {
+        if (self::is_bit_set($value, $sizeof * 8 - 1)) {
             $value = ~$value;
-            return -u\to_int($value) - 1;
+            return -self::to_int($value) - 1;
         }
 
-        return u\to_int($value);
+        return self::to_int($value);
     }
 
-    function fits_into_int(string $value): bool
+    public static function fits_into_int(string $value): bool
     {
         $value = \rtrim($value, "\0");
         $sizeof = \strlen($value);
@@ -65,7 +59,7 @@ namespace Arokettu\Unsigned
         return !$notFits;
     }
 
-    function from_hex(string $value, int $sizeof): string
+    public static function from_hex(string $value, int $sizeof): string
     {
         $strlen = \strlen($value);
         $hexsize = $sizeof * 2;
@@ -85,41 +79,41 @@ namespace Arokettu\Unsigned
         return \strrev(\hex2bin($value));
     }
 
-    function to_hex(string $value): string
+    public static function to_hex(string $value): string
     {
         return \bin2hex(\strrev($value));
     }
 
-    function from_base(string $value, int $base, int $sizeof): string
+    public static function from_base(string $value, int $base, int $sizeof): string
     {
         if ($base < 2 || $base > 36) {
             throw new \DomainException('$base must be between 2 and 36');
         }
 
-        $chars = \substr(i\ALPHABET, 0, $base);
+        $chars = \substr(self::_ALPHABET, 0, $base);
 
         if (!\preg_match("/^[{$chars}]+$/i", $value)) {
             throw new \DomainException('$value contains invalid digits');
         }
 
         if ($base === 16) {
-            return u\from_hex($value, $sizeof);
+            return self::from_hex($value, $sizeof);
         }
 
         $value = \strtolower($value);
         $result = \str_repeat("\0", $sizeof);
 
         for ($i = 0; $i < \strlen($value); $i++) {
-            $result = u\add_int(
-                u\mul_int($result, $base),
-                i\DIGIT_VALUE[$value[$i]]
+            $result = self::add_int(
+                self::mul_int($result, $base),
+                self::_DIGIT_VALUE[$value[$i]]
             );
         }
 
         return $result;
     }
 
-    function to_base(string $value, int $base): string
+    public static function to_base(string $value, int $base): string
     {
         if ($base < 2 || $base > 36) {
             throw new \DomainException('$base must be between 2 and 36');
@@ -130,27 +124,27 @@ namespace Arokettu\Unsigned
         $zero = \str_repeat("\0", \strlen($value));
 
         do {
-            list($value, $mod) = u\div_mod_int($value, $base);
-            $result .= i\ALPHABET[$mod];
+            list($value, $mod) = self::div_mod_int($value, $base);
+            $result .= self::_ALPHABET[$mod];
         } while ($value !== $zero);
 
         return \strrev($result);
     }
 
-    function from_dec(string $value, int $sizeof): string
+    public static function from_dec(string $value, int $sizeof): string
     {
-        return u\from_base($value, 10, $sizeof);
+        return self::from_base($value, 10, $sizeof);
     }
 
-    function to_dec(string $value): string
+    public static function to_dec(string $value): string
     {
-        return u\to_base($value, 10);
+        return self::to_base($value, 10);
     }
 
     /**
      * $value << $shift
      */
-    function shift_left(string $value, int $shift): string
+    public static function shift_left(string $value, int $shift): string
     {
         $sizeof = \strlen($value);
 
@@ -182,7 +176,7 @@ namespace Arokettu\Unsigned
     /**
      * $value >> $shift
      */
-    function shift_right(string $value, int $shift): string
+    public static function shift_right(string $value, int $shift): string
     {
         $sizeof = \strlen($value);
 
@@ -218,7 +212,7 @@ namespace Arokettu\Unsigned
     /**
      * a + b
      */
-    function add(string $a, string $b): string
+    public static function add(string $a, string $b): string
     {
         $sizeof = \strlen($a);
         $sizeofb = \strlen($b);
@@ -239,15 +233,15 @@ namespace Arokettu\Unsigned
     /**
      * a + int(b)
      */
-    function add_int(string $a, int $b): string
+    public static function add_int(string $a, int $b): string
     {
         $sizeof = \strlen($a);
 
         if ($b === 0) {
             return $a;
         }
-        if ($b > i\MAX_ADD) {
-            return u\add($a, u\from_int($b, $sizeof));
+        if ($b > self::_MAX_ADD) {
+            return self::add($a, self::from_int($b, $sizeof));
         }
 
         $r = $a;
@@ -267,51 +261,51 @@ namespace Arokettu\Unsigned
     /**
      * a - b
      */
-    function sub(string $a, string $b): string
+    public static function sub(string $a, string $b): string
     {
-        return u\add($a, u\neg($b));
+        return self::add($a, self::neg($b));
     }
 
     /**
      * a - int(b)
      */
-    function sub_int(string $a, int $b): string
+    public static function sub_int(string $a, int $b): string
     {
         // handle overflow on negative
         if ($b === \PHP_INT_MIN) {
-            return u\sub($a, u\from_int($b, \strlen($a)));
+            return self::sub($a, self::from_int($b, \strlen($a)));
         }
-        return u\add_int($a, -$b);
+        return self::add_int($a, -$b);
     }
 
     /**
      * int(a) - b
      */
-    function sub_int_rev(int $a, string $b): string
+    public static function sub_int_rev(int $a, string $b): string
     {
-        return u\add_int(u\neg($b), $a);
+        return self::add_int(self::neg($b), $a);
     }
 
     /**
      * -a
      */
-    function neg(string $a): string
+    public static function neg(string $a): string
     {
-        return u\add_int(~$a, 1);
+        return self::add_int(~$a, 1);
     }
 
     /**
      * a * b
      */
-    function mul(string $a, string $b): string
+    public static function mul(string $a, string $b): string
     {
-        return i\_raw_mul($a, $b, false);
+        return self::_raw_mul($a, $b, false);
     }
 
     /**
      * a * int(b)
      */
-    function mul_int(string $a, int $b): string
+    public static function mul_int(string $a, int $b): string
     {
         $sizeof = \strlen($a);
 
@@ -323,15 +317,15 @@ namespace Arokettu\Unsigned
             return $a;
         }
         if ($b === -1) {
-            return u\neg($a);
+            return self::neg($a);
         }
         // overflow for the next handler
-        if ($b === \PHP_INT_MIN || $b > i\MAX_MUL) {
-            return i\_raw_mul($a, u\from_int($b, $sizeof), true);
+        if ($b === \PHP_INT_MIN || $b > self::_MAX_MUL) {
+            return self::_raw_mul($a, self::from_int($b, $sizeof), true);
         }
         // we handle only positive, but we can move the 'sign' to the left
         if ($b < 0) {
-            return u\mul_int(u\neg($a), -$b);
+            return self::mul_int(self::neg($a), -$b);
         }
 
         $r = $a;
@@ -350,7 +344,7 @@ namespace Arokettu\Unsigned
      *
      * @return array [div -> string, mod -> string]
      */
-    function div_mod(string $a, string $b, bool $forceSlow = false): array
+    public static function div_mod(string $a, string $b, bool $forceSlow = false): array
     {
         $sizeof = \strlen($a);
         $sizeofb = \strlen($b);
@@ -360,7 +354,7 @@ namespace Arokettu\Unsigned
 
         // special cases
         $zero = \str_repeat("\0", $sizeof);
-        $compare = u\compare($a, $b);
+        $compare = self::compare($a, $b);
         // if a < b, result is 0 and modulo is a
         if ($compare < 0) {
             return [$zero, $a];
@@ -380,32 +374,32 @@ namespace Arokettu\Unsigned
             return [$a, $zero];
         }
         // for pow2 just cut the required bits
-        $b1 = u\add_int($b, -1);
+        $b1 = self::add_int($b, -1);
         if (($b & $b1) === $zero) {
             $i = 0;
-            while (!u\is_bit_set($b, $i)) {
+            while (!self::is_bit_set($b, $i)) {
                 $i++;
             }
             return [
-                u\shift_right($a, $i),
+                self::shift_right($a, $i),
                 $a & $b1,
             ];
         }
         // if we're lucky to have a small $b
-        if (!$forceSlow && u\fits_into_int($b)) {
-            $bi = u\to_int($b);
-            if ($bi <= i\MAX_DIV) {
-                list($div, $mod) = u\div_mod_int($a, u\to_int($b));
+        if (!$forceSlow && self::fits_into_int($b)) {
+            $bi = self::to_int($b);
+            if ($bi <= self::_MAX_DIV) {
+                list($div, $mod) = self::div_mod_int($a, self::to_int($b));
                 return [
                     $div,
-                    u\from_int($mod, $sizeof),
+                    self::from_int($mod, $sizeof),
                 ];
             }
         }
 
         $b = \rtrim($b, "\0"); // only significant bytes
         $bZero = $b . "\0";
-        $bAdd = u\neg($bZero);
+        $bAdd = self::neg($bZero);
         $sizeofFrame = \strlen($b);
 
         $r = $zero;
@@ -415,8 +409,8 @@ namespace Arokettu\Unsigned
         while ($i--) {
             $m = $a[$i] . $m;
             $chr = 0;
-            while (u\compare($m, $bZero) >= 0) {
-                $m = u\add($m, $bAdd);
+            while (self::compare($m, $bZero) >= 0) {
+                $m = self::add($m, $bAdd);
                 $chr++;
             }
             $r[$i] = \chr($chr);
@@ -431,7 +425,7 @@ namespace Arokettu\Unsigned
      *
      * @return array [div -> string, mod -> int]
      */
-    function div_mod_int(string $a, int $b): array
+    public static function div_mod_int(string $a, int $b): array
     {
         $sizeof = \strlen($a);
 
@@ -449,9 +443,9 @@ namespace Arokettu\Unsigned
             return [$a, 0];
         }
         // fits into int, just calculate natively
-        if (u\fits_into_int($a)) {
-            $ai = u\to_int($a);
-            return [u\from_int(\intdiv($ai, $b), $sizeof), $ai % $b];
+        if (self::fits_into_int($a)) {
+            $ai = self::to_int($a);
+            return [self::from_int(\intdiv($ai, $b), $sizeof), $ai % $b];
         }
         // for pow2 just cut the required bits
         if (($b & ($b - 1)) === 0) {
@@ -460,14 +454,14 @@ namespace Arokettu\Unsigned
                 $i++;
             }
             return [
-                u\shift_right($a, $i),
-                u\to_int($a & u\from_int($b - 1, $sizeof)),
+                self::shift_right($a, $i),
+                self::to_int($a & self::from_int($b - 1, $sizeof)),
             ];
         }
         // catch possible overflow
-        if ($b > i\MAX_DIV) {
-            $divmod = u\div_mod($a, u\from_int($b, $sizeof), true);
-            return [$divmod[0], u\to_int($divmod[1])];
+        if ($b > self::_MAX_DIV) {
+            $divmod = self::div_mod($a, self::from_int($b, $sizeof), true);
+            return [$divmod[0], self::to_int($divmod[1])];
         }
 
         $mod = 0;
@@ -485,15 +479,15 @@ namespace Arokettu\Unsigned
     /**
      * a / b
      */
-    function div(string $a, string $b): string
+    public static function div(string $a, string $b): string
     {
-        return u\div_mod($a, $b)[0];
+        return self::div_mod($a, $b)[0];
     }
 
     /**
      * a / int(b)
      */
-    function div_int(string $a, int $b): string
+    public static function div_int(string $a, int $b): string
     {
         // can't handle negative, convert to unsigned first
         // custom message
@@ -503,13 +497,13 @@ namespace Arokettu\Unsigned
             );
         }
 
-        return u\div_mod_int($a, $b)[0];
+        return self::div_mod_int($a, $b)[0];
     }
 
     /**
      * a % b
      */
-    function mod(string $a, string $b): string
+    public static function mod(string $a, string $b): string
     {
         $sizeof = \strlen($a);
         $sizeofb = \strlen($b);
@@ -518,7 +512,7 @@ namespace Arokettu\Unsigned
         }
 
         // special cases
-        $compare = u\compare($a, $b);
+        $compare = self::compare($a, $b);
         // if a < b, entire a is modulo
         if ($compare < 0) {
             return $a;
@@ -539,23 +533,23 @@ namespace Arokettu\Unsigned
             return $zero;
         }
         // for pow2 just cut the required bits
-        $b1 = u\add_int($b, -1);
+        $b1 = self::add_int($b, -1);
         if (($b & $b1) === $zero) {
             return $a & $b1;
         }
         // if we're lucky to have a small $b
-        if (u\fits_into_int($b)) {
-            return u\from_int(u\mod_int($a, u\to_int($b)), $sizeof);
+        if (self::fits_into_int($b)) {
+            return self::from_int(self::mod_int($a, self::to_int($b)), $sizeof);
         }
 
         // do a slow algo
-        return u\div_mod($a, $b)[1];
+        return self::div_mod($a, $b)[1];
     }
 
     /**
      * a % int(b) -> int
      */
-    function mod_int(string $a, int $b): int
+    public static function mod_int(string $a, int $b): int
     {
         $sizeof = \strlen($a);
 
@@ -573,17 +567,17 @@ namespace Arokettu\Unsigned
             return 0;
         }
         // fits into int, just calculate natively
-        if (u\fits_into_int($a)) {
-            $ai = u\to_int($a);
+        if (self::fits_into_int($a)) {
+            $ai = self::to_int($a);
             return $ai % $b;
         }
         // for pow2 just cut the required bits
         if (($b & ($b - 1)) === 0) {
-            return u\to_int($a & u\from_int($b - 1, $sizeof));
+            return self::to_int($a & self::from_int($b - 1, $sizeof));
         }
         // catch possible overflow
-        if ($b > i\MAX_DIV) {
-            return u\to_int(u\div_mod($a, u\from_int($b, $sizeof), true)[1]);
+        if ($b > self::_MAX_DIV) {
+            return self::to_int(self::div_mod($a, self::from_int($b, $sizeof), true)[1]);
         }
 
         $mod = 0;
@@ -599,7 +593,7 @@ namespace Arokettu\Unsigned
     /**
      * a <=> b
      */
-    function compare(string $a, string $b): int
+    public static function compare(string $a, string $b): int
     {
         $sizeof = \strlen($a);
         $sizeofb = \strlen($b);
@@ -617,7 +611,7 @@ namespace Arokettu\Unsigned
         return 0;
     }
 
-    function set_bit(string $a, int $bit): string
+    public static function set_bit(string $a, int $bit): string
     {
         $sizeof = \strlen($a);
         if ($bit < 0) {
@@ -636,7 +630,7 @@ namespace Arokettu\Unsigned
         return $a;
     }
 
-    function unset_bit(string $a, int $bit): string
+    public static function unset_bit(string $a, int $bit): string
     {
         $sizeof = \strlen($a);
         if ($bit < 0) {
@@ -655,7 +649,7 @@ namespace Arokettu\Unsigned
         return $a;
     }
 
-    function is_bit_set(string $a, int $bit): bool
+    public static function is_bit_set(string $a, int $bit): bool
     {
         $sizeof = \strlen($a);
         if ($bit < 0) {
@@ -671,22 +665,15 @@ namespace Arokettu\Unsigned
 
         return (\ord($a[$byte]) & $bitmask) !== 0;
     }
-}
-
-/** @internal */
-namespace Arokettu\Unsigned\Internal
-{
-    use Arokettu\Unsigned as u;
-    use Arokettu\Unsigned\Internal as i;
 
     /**
      * @internal
      */
-    const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyz';
+    const _ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyz';
     /**
      * @internal
      */
-    const DIGIT_VALUE = [
+    const _DIGIT_VALUE = [
         0 => 0, 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 'a' => 10, 'b' => 11, 'c' => 12,
         'd' => 13, 'e' => 14, 'f' => 15, 'g' => 16, 'h' => 17, 'i' => 18, 'j' => 19, 'k' => 20, 'l' => 21, 'm' => 22,
         'n' => 23, 'o' => 24, 'p' => 25, 'q' => 26, 'r' => 27, 's' => 28, 't' => 29, 'u' => 30, 'v' => 31, 'w' => 32,
@@ -697,22 +684,22 @@ namespace Arokettu\Unsigned\Internal
      * @internal
      * Max integer that can be added to a byte without overflowing
      */
-    const MAX_ADD = \PHP_INT_MAX - 255;
+    const _MAX_ADD = \PHP_INT_MAX - 255;
     /**
      * @internal
      * Max integer that can be multiplied by a byte without overflowing (+ carry)
      */
-    const MAX_MUL = (\PHP_INT_MAX >> 8) + 1;
+    const _MAX_MUL = (\PHP_INT_MAX >> 8) + 1;
     /**
      * @internal
      * Max dividend that can be shifted by a byte length without overflowing
      */
-    const MAX_DIV = \PHP_INT_MAX >> 8;
+    const _MAX_DIV = \PHP_INT_MAX >> 8;
 
     /**
      * @internal
      */
-    function _raw_mul(string $a, string $b, bool $forceSlow): string
+    private static function _raw_mul(string $a, string $b, bool $forceSlow): string
     {
         $sizeof = \strlen($a);
         $sizeofb = \strlen($b);
@@ -720,27 +707,27 @@ namespace Arokettu\Unsigned\Internal
             throw new \DomainException("Arguments must be the same size, $sizeof and $sizeofb bytes given");
         }
         // if we're lucky to have a small $a
-        if (!$forceSlow && u\fits_into_int($a)) {
-            $ai = u\to_int($a);
-            if ($ai <= i\MAX_MUL) {
-                return u\mul_int($b, $ai);
+        if (!$forceSlow && self::fits_into_int($a)) {
+            $ai = self::to_int($a);
+            if ($ai <= self::_MAX_MUL) {
+                return self::mul_int($b, $ai);
             }
         }
         // or $b
-        if (!$forceSlow && u\fits_into_int($b)) {
-            $bi = u\to_int($b);
-            if ($bi <= i\MAX_MUL) {
-                return u\mul_int($a, $bi);
+        if (!$forceSlow && self::fits_into_int($b)) {
+            $bi = self::to_int($b);
+            if ($bi <= self::_MAX_MUL) {
+                return self::mul_int($a, $bi);
             }
         }
 
-        return \PHP_INT_SIZE >= 8 ? i\_raw_mul64($a, $b, $sizeof) : i\_raw_mul32($a, $b, $sizeof);
+        return \PHP_INT_SIZE >= 8 ? self::_raw_mul64($a, $b, $sizeof) : self::_raw_mul32($a, $b, $sizeof);
     }
 
     /**
      * @internal
      */
-    function _raw_mul32(string $a, string $b, int $sizeof): string
+    private static function _raw_mul32(string $a, string $b, int $sizeof): string
     {
         $newval = \str_repeat("\0", $sizeof);
 
@@ -762,7 +749,7 @@ namespace Arokettu\Unsigned\Internal
     /**
      * @internal
      */
-    function _raw_mul64(string $a, string $b, int $sizeof): string
+    private static function _raw_mul64(string $a, string $b, int $sizeof): string
     {
         // we can safely process 3 (PHP_INT_SIZE-1 div 2) on 64-bit systems
 
